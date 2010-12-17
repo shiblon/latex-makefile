7902e2673696ff276474b31087b0aded6edcadb0
Chris Monson <shiblon@gmail.com>
2010-12-17 10:11:30 -0500

    Updated version test to disallow Python 3

--- util/ensure_version.py
+++ util/ensure_version.py
@@ -1,6 +1,6 @@
 from __future__ import print_function
 import sys
 
-if sys.version_info < (2, 6):
-  print("You must run this with Python 2.6 or higher.", file=sys.stderr)
+if sys.version_info < (2, 6) or sys.version_info >= (3,):
+  print("You must run this with Python version > 2.6 and < 3.0", file=sys.stderr)
   sys.exit(-1)
