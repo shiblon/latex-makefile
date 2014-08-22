25641d0595dddf7266987c25f4230721648ce752
Chris Monson <shiblon@gmail.com>
2014-08-22 08:38:05 -0400

    Updated tests to allow local styles in texmf directories.

--- test-gen-logs.py
+++ test-gen-logs.py
@@ -18,6 +18,9 @@ else:
   initial_dir = "test"
 
 for dirpath, dirnames, filenames in os.walk(initial_dir):
+  if os.path.basename(dirpath) == "texmf":
+    continue
+
   for fname in filenames:
     name, ext = os.path.splitext(fname)
     if ext != ".tex":
@@ -25,12 +28,18 @@ for dirpath, dirnames, filenames in os.walk(initial_dir):
 
     print("Building {0}".format(os.path.join(dirpath, name)))
 
+    env = os.environ.copy()
+    if "texmf" in dirnames:
+      env["TEXINPUTS"] = "%s::" % os.path.join(dirpath, "texmf")
+
     for command in commands:
+      print "Running %r in %r" % (command, dirpath)
       ret = subprocess.call(args=(command,
                                   "-interaction=batchmode",
                                   "-file-line-error",
                                   name),
-                            cwd=dirpath)
+                            cwd=dirpath,
+                            env=env)
       subprocess.call(args=("mv", "-f", "{0}.log".format(name),
                             "log.{0}--{1}".format(name, command)),
                       cwd=dirpath)
