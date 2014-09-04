886c8002b85d37865b0ea4518a1b6b717a0b7ffe
Chris Monson <shiblon@gmail.com>
2014-09-04 15:08:53 -0400

    Changed log generation script to look for local texmf directories. Added generation and preservation of .fls files. graphic-in-style now produces sensible logs.

--- test-gen-logs.py
+++ test-gen-logs.py
@@ -17,9 +17,15 @@ if len(sys.argv) == 2:
 else:
   initial_dir = "test"
 
+last_dir = None
+
 for dirpath, dirnames, filenames in os.walk(initial_dir):
   if os.path.basename(dirpath) == "texmf":
     continue
+
+  env = os.environ.copy()
+  if "texmf" in dirnames:
+    env["TEXINPUTS"] = "./texmf::"
 
   for fname in filenames:
     name, ext = os.path.splitext(fname)
@@ -28,20 +34,22 @@ for dirpath, dirnames, filenames in os.walk(initial_dir):
 
     print("Building {0}".format(os.path.join(dirpath, name)))
 
-    env = os.environ.copy()
-    if "texmf" in dirnames:
-      env["TEXINPUTS"] = "%s::" % os.path.join(dirpath, "texmf")
-
     for command in commands:
+      print env["TEXINPUTS"]
       print "Running %r in %r" % (command, dirpath)
       ret = subprocess.call(args=(command,
                                   "-interaction=batchmode",
                                   "-file-line-error",
+                                  "-recorder",
                                   name),
                             cwd=dirpath,
                             env=env)
       subprocess.call(args=("mv", "-f", "{0}.log".format(name),
                             "log.{0}--{1}".format(name, command)),
+                      cwd=dirpath)
+
+      subprocess.call(args=("mv", "-f", "{0}.fls".format(name),
+                            "fls.{0}--{1}".format(name, command)),
                       cwd=dirpath)
 
       rm_files = ["{0}.{1}".format(name, x) for x in rm_ext]
